eb9fefad8a832697805212e5aa165bc985bd52c3
Shinji Tanaka <stanaka@mercari.com>
2019-11-16 07:52:22 +0900

    initial commit

---
+++ src/utils.ts
@@ -0,0 +1,13 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+
+export function getBasePath(): string {
+    const basePath = vscode.workspace.getConfiguration('quickNotes').get('basePath', "");
+
+    if (basePath[0] === '~') {
+        if (process.env.HOME !== undefined) {
+            return path.join(process.env.HOME, basePath.slice(1));
+        }
+    }
+    return basePath;
+}
