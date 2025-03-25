0a99a3cee2d0746a671d06e682940ea5bac65113
Shinji Tanaka <shinji.tanaka@gmail.com>
2025-03-25 20:09:48 +0900

    add todoCount property to show counts of unchecked and checked TODOs

--- src/handlerSaveEvent.ts
+++ src/handlerSaveEvent.ts
@@ -23,6 +23,12 @@ export const handlerSaveEvent = (event: vscode.TextDocumentWillSaveEvent) => {
     }
 
     const fileContents = document.getText()
+
+    // Count unchecked and checked checkboxes in the file contents
+    const uncheckedCount = (fileContents.match(/\[ \]/g) || []).length
+    const checkedCount = (fileContents.match(/\[x\]/g) || []).length
+    const todoCount = `${checkedCount}/${uncheckedCount + checkedCount}`
+
     if (matter.test(fileContents) == false) {
         console.log(
             `The file: ${document.uri.path} doesn't contain front-matter`
@@ -38,12 +44,35 @@ export const handlerSaveEvent = (event: vscode.TextDocumentWillSaveEvent) => {
     const modifiedProperty = vscode.workspace
         .getConfiguration('quickNotes')
         .get('lastModifiedProperty', '')
+    const todoCountProperty = vscode.workspace
+        .getConfiguration('quickNotes')
+        .get('todoCountProperty', 'todoCount')
 
     const re = new RegExp('^' + modifiedProperty + ':.+$', 'gm')
-    const updatedMatter = properties.matter.replace(
+    let updatedMatter = properties.matter.replace(
         re,
         modifiedProperty + ': ' + moment().format(dateFormat)
     )
+
+    // Update todoCount property in front matter only if there are checkboxes
+    const totalCount = uncheckedCount + checkedCount
+    if (totalCount > 0) {
+        const todoCountRe = new RegExp('^' + todoCountProperty + ':.+$', 'gm')
+        if (todoCountRe.test(updatedMatter)) {
+            // Replace existing todoCount property
+            updatedMatter = updatedMatter.replace(
+                todoCountRe,
+                todoCountProperty + ': ' + todoCount
+            )
+        } else {
+            // Add todoCount property if it doesn't exist
+            if (!updatedMatter.endsWith('\n')) {
+                updatedMatter += '\n'
+            }
+            updatedMatter += todoCountProperty + ': ' + todoCount
+        }
+    }
+
     const updatedDoc = '---' + updatedMatter + '\n---\n' + properties.content
     //const updatedDoc = matter.stringify(properties.content, properties.data)
 
